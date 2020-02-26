51e22f679958a53aa74e9423d2bd9398e216c032
nykekana <45674574+nykekana@users.noreply.github.com>
2020-02-26 06:04:06 -0500

    change model path

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ CLASSES = {
 };
 
 const MODEL_PATH =
-    'model.json';
+    'landmark_model.json';
 
 const IMAGE_SIZE = 192;
-const TOPK_PREDICTIONS = 5;
+const TOPK_PREDICTIONS =5;
 
 let my_model;
 const demo = async () => {
